4edbe38ede8c00501906795ddd90a4c04219a370
allanTous23 <129600223+allanTous23@users.noreply.github.com>
2025-02-14 19:15:13 +0000

    Rename invalid file

--- work/predict.py
+++ work/predict.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Charger le modèle
-with open("models", "rb") as f:
+with open("house_price_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 @app.route("/predict", methods=["POST"])
