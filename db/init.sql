6919afa5d93c32b76935f5ab51fde4c2c51442d2
franklin kana nguedia <fknguedia@gmail.com>
2025-02-27 21:51:38 +0100

    modification

--- db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS predictions;
-
-USE predictions;
-
-CREATE TABLE IF NOT EXISTS predictions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    input_data TEXT ,
-    output_data TEXT 
-);
